ec338e0c3171d2adcfab8ef46c4945a9f7be43ac
tomvieilledent <tomvieilledent@gmail.com>
2026-01-12 15:11:56 +0100

    Refactor last digit calculation in 1-last_digit.py for improved readability

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number < 0:
-    positiveNumber = -number
-    lastDigit = positiveNumber % 10
-    lastDigit = -lastDigit
-lastDigit = number % 10
+lastDigit = number % 10 if number > 0 else -(-number % 10)
 if lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif lastDigit == 0:
