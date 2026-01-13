3f3a0b3cfca18bae0fc5db9aae361acf8669daf8
tomvieilledent <tomvieilledent@gmail.com>
2026-01-13 15:17:18 +0100

    Refactor argument count output logic in 2-args.py for clarity

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 import sys
 
 if __name__ == "__main__":
-    print("{} arguments.".format(len(sys.argv) - 1))
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     for count in range(1, len(sys.argv)):
         print("{}: {}".format(count, sys.argv[count]))
