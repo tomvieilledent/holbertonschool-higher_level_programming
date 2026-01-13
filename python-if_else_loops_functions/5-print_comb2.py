2eba316492443e43c93a46aee1865a3ab2a08e95
tomvieilledent <tomvieilledent@gmail.com>
2026-01-13 10:09:51 +0100

    Fix string formatting in number printing to use format method consistently

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(00, 100):
     if number not in [99]:
-        print(f"{number:02d}", end=", ")
+        print("{:02d}".format(number), end=", ")
     else:
-        print(number)
+        print("{}".format(number))
