6ef61400834c684e5c67f95381f0ff57f938492a
tomvieilledent <tomvieilledent@gmail.com>
2026-01-14 16:16:51 +0100

    Refactor uppercase function to convert input string to uppercase using ASCII values

---
+++ python-if_else_loops_functions/8-uppercase_flo.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str = str.swapcase(str.lower())
+    print("{}".format(str))
---
+++ python-if_else_loops_functions/8-uppercase_tom.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str = str.lower()
+    str = str.swapcase()
+    print("{}".format(str))
