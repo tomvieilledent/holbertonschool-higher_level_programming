4e22a655f1cf4cde4f99512508cc7aeadcccae41
tomvieilledent <tomvieilledent@gmail.com>
2026-01-14 11:11:00 +0100

    Add 4-hidden_discovery.py to list non-magic attributes from hidden_4 module

---
+++ 4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import hidden_4
+
+if __name__ == "__main__":
+
+    name = dir(hidden_4)
+    sortedName = sorted(name)
+    for nameList in sortedName:
+        if nameList.startswith("__"):
+            continue
+        print(nameList)
