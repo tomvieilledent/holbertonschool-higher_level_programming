bffdcf517f3b7ab6e5df4ab8a2057fe9cd5330e7
Vieilledent Tom <tomvieilledent@gmail.com>
2026-01-14 11:38:32 +0100

    Fix missing newline at end of 4-hidden_discovery.py
    Added a newline at the end of the file for consistency.

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,10 +2,11 @@
 import hidden_4
 
 if __name__ == "__main__":
-
+    
     names = dir(hidden_4)
     names_alpha = sorted(names)
     for name in names_alpha:
         if name.startswith("__"):
             continue
         print(name)
+        
