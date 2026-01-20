b9c449caa0558c5a11cb53cfa6c08281a9a14015
tomvieilledent <tomvieilledent@gmail.com>
2026-01-20 11:26:17 +0100

    Fix print_matrix_integer function to handle empty matrix case

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
     for row in matrix:
         for pos in range(len(row)):
             if pos == len(row)-1:
