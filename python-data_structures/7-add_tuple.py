6432464b41b1ea6fd7de8dea4f116701d67319ce
tomvieilledent <tomvieilledent@gmail.com>
2026-01-20 11:57:01 +0100

    Refactor add_tuple function to improve readability and handle tuple length variations

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    tuple_a0 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    tuple_b0 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    tuple_a1 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    tuple_b1 = tuple_b[1] if len(tuple_b) >= 2 else 0
+    new_tuple = tuple_a0 + tuple_b0, tuple_a1 + tuple_b1
+    return new_tuple
