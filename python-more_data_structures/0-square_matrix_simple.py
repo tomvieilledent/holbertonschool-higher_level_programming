10dc7fe3bc1c541aa76acbb28c458213cab3577a
tomvieilledent <tomvieilledent@gmail.com>
2026-01-20 15:15:04 +0100

    Refactor comments for clarity and formatting in square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,11 @@ def square_matrix_simple(matrix=[]):
     new_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
     return new_matrix
 
-# map extérieur  → lignes                    list(map(lambda row)) donne une matrice
-# map intérieur  → éléments                  list(map(lambda x)) donne une ligne
+# map extérieur  → lignes
+#   list(map(lambda row)) donne une matrice
+# map intérieur  → éléments
+#   list(map(lambda x)) donne une ligne
+
 # lambda row     → transforme une ligne
 # lambda x       → transforme un élément
 # list()         → matérialise le résultat
