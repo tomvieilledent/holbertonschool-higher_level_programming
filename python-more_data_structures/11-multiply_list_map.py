582728e23466d8eef4e51b1e9f0681f8daeeacc4
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 10:40:55 +0100

    Implement multiply_list_map function to multiply each element in a list by a given number

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        new_list[i] *= number
+    return new_list
