86a09aebfaa8555162dd44e3c1557ed0d1c3f399
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 10:29:10 +0100

    Remove unnecessary whitespace in best_score function

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -10,5 +10,5 @@ def best_score(a_dictionary):
         if a_dictionary[key] > best_value:
             best_value = a_dictionary[key]
             best_key = key
-            
+
     return best_key
