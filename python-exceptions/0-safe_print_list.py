7e164bd1c9ed99037eaf814a0ca99a1c6086aba5
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 13:46:09 +0100

    Simplify print statement in safe_print_list function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end="")
+            print(my_list[i], end="")
             i += 1
         print("")
     except:
