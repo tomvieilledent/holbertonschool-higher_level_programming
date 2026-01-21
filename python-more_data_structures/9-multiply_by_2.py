9111bcdb3f369fbb653e25a13f4d19c67d27fe01
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 10:07:52 +0100

    Refactor multiply_by_2 function to use a copy of the dictionary for modifications

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] += a_dictionary[key]
-    return a_dictionary
+    new_dictionary = a_dictionary.copy()
+    for key in new_dictionary:
+        new_dictionary[key] += new_dictionary[key]
+    return new_dictionary
