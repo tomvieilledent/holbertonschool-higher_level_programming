91b58d130edc8b2e1eba8bda7f8e1f019a6c9aeb
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 12:44:24 +0100

    Validate input for roman_to_int function to ensure it is a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
-    if roman_string == None:
-        return roman_string
+    if roman_string is None or not isinstance(roman_string, str):
+        return None
 
     roman_number = {"I": 1, "V": 5, "X": 10,
                     "L": 50, "C": 100, "D": 500, "M": 1000}
