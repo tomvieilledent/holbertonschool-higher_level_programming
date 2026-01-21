7e769ce9eb074eb6f236e26ef3d202923ab421de
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 10:49:09 +0100

    Refactor multiply_list_map function to use map for element multiplication

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    for i in range(len(new_list)):
-        new_list[i] *= number
+    new_list = list(map(lambda x: x * number, my_list))
     return new_list
