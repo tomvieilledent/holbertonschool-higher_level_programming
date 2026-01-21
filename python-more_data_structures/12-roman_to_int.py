cc6037a1e64c067ef7bcfec3c9aa3815e539135e
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 12:53:03 +0100

    Refactor conditional statement for readability in roman_to_int function

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,8 @@ def roman_to_int(roman_string):
     result = 0
 
     for i in range(len(roman_string)):
-        if i < len(roman_string) - 1 and roman_number[roman_string[i]] < roman_number[roman_string[i + 1]]:
+        if (i < len(roman_string) - 1 and
+            roman_number[roman_string[i]] < roman_number[roman_string[i + 1]]):
             result -= roman_number[roman_string[i]]
         else:
             result += roman_number[roman_string[i]]
