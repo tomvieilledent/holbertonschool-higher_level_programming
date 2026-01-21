5a38d9c1be72f108fb2e08723352bda4c402ba42
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 13:51:14 +0100

    Refactor safe_print_list function for improved readability

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             i += 1
         print("")
-    except:
+    except IndexError:
         print("")
     return i
