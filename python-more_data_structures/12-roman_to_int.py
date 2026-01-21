36da277dd4715dc87cec213cb333643ec9f604a1
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 12:55:24 +0100

    Fix variable name for consistency in roman_to_int function

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,15 +4,15 @@ def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
 
-    roman_number = {"I": 1, "V": 5, "X": 10,
+    roman_num = {"I": 1, "V": 5, "X": 10,
                     "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
 
     for i in range(len(roman_string)):
         if (i < len(roman_string) - 1 and
-            roman_number[roman_string[i]] < roman_number[roman_string[i + 1]]):
-            result -= roman_number[roman_string[i]]
+                roman_num[roman_string[i]] < roman_num[roman_string[i + 1]]):
+            result -= roman_num[roman_string[i]]
         else:
-            result += roman_number[roman_string[i]]
+            result += roman_num[roman_string[i]]
 
     return result
