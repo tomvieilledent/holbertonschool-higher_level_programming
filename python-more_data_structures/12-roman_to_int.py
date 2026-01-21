c32241f01b2bafafef3ad87cc17f3c5462e655ca
tomvieilledent <tomvieilledent@gmail.com>
2026-01-21 12:56:26 +0100

    Fix indentation for consistency in roman_to_int function

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
         return 0
 
     roman_num = {"I": 1, "V": 5, "X": 10,
-                    "L": 50, "C": 100, "D": 500, "M": 1000}
+                 "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
 
     for i in range(len(roman_string)):
