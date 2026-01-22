3d4b3f73cbec257e41842a4b9d6e41f4dc677119
tomvieilledent <tomvieilledent@gmail.com>
2026-01-22 11:10:37 +0100

    Fix formatting in add_integer function docstring and code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,14 +7,14 @@ This module contains a function that adds two integers.
 def add_integer(a, b=98):
     """
     Adds two integers.
-    
+
     Args:
         a: Must be an integer or float
         b: Must be an integer or float (default 98)
-    
+
     Returns:
         The addition of a and b as integer
-    
+
     Raises:
         TypeError: If a or b is not an integer or float
     """
@@ -22,5 +22,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
