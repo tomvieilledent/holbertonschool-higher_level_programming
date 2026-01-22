7cdb21f087346de84500ec310e15cc5068a432ee
tomvieilledent <tomvieilledent@gmail.com>
2026-01-22 13:34:33 +0100

    Refactor matrix_divided function to improve type checking for matrix input

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,11 +2,14 @@
 def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    elif not isinstance(div, (int, float)):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (array of arrays of integers/floats)")
     for i in range(len(matrix)):
         if len(matrix[0]) != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
+    
 
     new_matrix = list(map(lambda row: list(
         map(lambda x: round(x / div, 2), row)), matrix))
