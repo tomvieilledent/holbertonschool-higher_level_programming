36c152bef5e1fe2817af9712eaed3ac262057bf2
tomvieilledent <tomvieilledent@gmail.com>
2026-01-22 10:51:37 +0100

    Implement add_integer function with type checks and error handling

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    try:
+        if isinstance(a, (float, int)) and isinstance(b, (float, int)):
+            result = int(a) + int(b)
+        else:
+            if not isinstance(a, (int, float)):
+                raise TypeError("a must be an integer")
+            if not isinstance(b, (int, float)):
+                raise TypeError("b must be an integer")
+
+    except TypeError:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+    return result
