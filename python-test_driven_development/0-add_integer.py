acdd97004978c97427d27c45fa34fcf69b6fa38e
tomvieilledent <tomvieilledent@gmail.com>
2026-01-22 10:57:08 +0100

    Refactor add_integer function to improve readability and maintain error handling

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    try:
-        if isinstance(a, (float, int)) and isinstance(b, (float, int)):
-            result = int(a) + int(b)
-        else:
-            if not isinstance(a, (int, float)):
-                raise TypeError("a must be an integer")
-            if not isinstance(b, (int, float)):
-                raise TypeError("b must be an integer")
+"""
+This module contains a function that adds two integers.
+"""
 
-    except TypeError:
-        if not isinstance(a, (int, float)):
-            raise TypeError("a must be an integer")
-        if not isinstance(b, (int, float)):
-            raise TypeError("b must be an integer")
-    return result
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+    
+    Args:
+        a: Must be an integer or float
+        b: Must be an integer or float (default 98)
+    
+    Returns:
+        The addition of a and b as integer
+    
+    Raises:
+        TypeError: If a or b is not an integer or float
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    return int(a) + int(b)
