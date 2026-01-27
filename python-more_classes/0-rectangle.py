efd948444116b27a58abf54d3c9bf248d30a8e09
tomvieilledent <tomvieilledent@gmail.com>
2026-01-27 09:45:25 +0100

    Implement Rectangle class with size attribute and initialization method

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class for demonstration purposes.
+"""
+
+class Rectangle:
+    """
+    Represents an empty rectangle with no attributes or methods.
+    """
+    pass
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with a private size attribute.
+"""
+
+class Rectangle:
+    """
+    Represents a square with a private size attribute.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance with the given size.
+        Args:
+            size: The size of the square (integer).
+        """
+        self.__size = size
