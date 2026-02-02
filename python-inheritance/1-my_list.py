6b4b28d4f75f8c3d2a5e317fef3cecd0e5f7f4c6
tomvieilledent <tomvieilledent@gmail.com>
2026-02-02 10:38:04 +0100

    Remove docstrings from MyList class and print_sorted method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-"""
-This module defines MyList class that inherits from list.
-"""
-
-
 class MyList(list):
-    """
-    MyList class that inherits from list with a print_sorted method.
-    """
 
     def print_sorted(self):
-        """
-        Print the list in ascending sorted order.
-        """
         print(sorted(self))
 
