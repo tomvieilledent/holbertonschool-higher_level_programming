04e835842140cffeba6d6374b5a1780225c7453b
tomvieilledent <tomvieilledent@gmail.com>
2026-02-02 15:20:21 +0100

    Fix string representation in Rectangle class and add Square class with proper initialization

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -19,7 +19,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
     
     def __str__(self):
-        return ("Rectangle] {}/{}".format(self.__width, self.__height))
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
     
     def area(self):
         return self.__width * self.__height
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -23,3 +23,13 @@ class Rectangle(BaseGeometry):
     
     def area(self):
         return self.__width * self.__height
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
+    
