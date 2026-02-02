793c8b6ef379b650ff618f83f7cfbd78d33949a6
tomvieilledent <tomvieilledent@gmail.com>
2026-02-02 15:27:44 +0100

    Fix Square class initialization by validating size before assignment

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -29,8 +29,9 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
 
     def __init__(self, size):
-        super().__init__(size, size)
         self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
 
     def __str__(self):
         return ("[Square] {}/{}".format(self.__size, self.__size))
