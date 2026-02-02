db21cccdb77a1bc9764831a1a3e9062564abdc89
tomvieilledent <tomvieilledent@gmail.com>
2026-02-02 15:32:15 +0100

    Refactor Square class initialization by moving size assignment after calling super constructor

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -29,6 +29,4 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
 
     def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
