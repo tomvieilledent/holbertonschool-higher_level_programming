a55447a168208648ea51ca5e3828a58307e8334c
tomvieilledent <tomvieilledent@gmail.com>
2026-02-02 14:15:58 +0100

    Remove unnecessary pass statement in BaseGeometry class

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class BaseGeometry:
-    pass
 
     def area(self):
         raise Exception("area() is not implemented")
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 class BaseGeometry:
-    pass
 
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+    
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
