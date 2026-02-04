efb3e9d2a649fcafeb5736c9c790f274cccd330e
tomvieilledent <tomvieilledent@gmail.com>
2026-02-04 08:00:18 +0100

    Fix integer validation in integer_validator to exclude boolean values

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validates that value is a positive integer."""
-        if not isinstance(value, int) or type(value) is bool:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
