55a2ffb5d1c79a9c36f5866d362550eb1ab3efaf
tomvieilledent <tomvieilledent@gmail.com>
2026-02-04 08:07:00 +0100

    Fix formatting by removing unnecessary blank lines in task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -4,6 +4,7 @@
 from abc import ABC, abstractmethod
 from math import pi
 
+
 class Shape(ABC):
     """Abstract base class for geometric shapes."""
     @abstractmethod
@@ -16,6 +17,7 @@ class Shape(ABC):
         """Returns the perimeter of the shape."""
         pass
 
+
 class Circle(Shape):
     """Circle class that inherits from Shape."""
 
@@ -26,10 +28,11 @@ class Circle(Shape):
     def area(self):
         """Returns the area of the circle."""
         return pi * (self.radius ** 2)
-    
+
     def perimeter(self):
         """Returns the perimeter of the circle."""
         return 2 * pi * self.radius
+
 
 class Rectangle(Shape):
     """Rectangle class that inherits from Shape."""
@@ -42,14 +45,13 @@ class Rectangle(Shape):
     def area(self):
         """Returns the area of the rectangle."""
         return self.width * self.height
-    
+
     def perimeter(self):
         """Returns the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
-    
+
+
 def shape_info(shape):
     """Prints the area and perimeter of a shape."""
     print("Area: {}".format(shape.area()))
     print("Perimeter: {}".format(shape.perimeter()))
-    
-
