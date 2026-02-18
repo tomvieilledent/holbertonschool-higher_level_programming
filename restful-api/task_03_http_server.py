f0b7a20fd2c4131b1ea6fb3bb9f8741fb930e7fd
tomvieilledent <tomvieilledent@gmail.com>
2026-02-18 14:28:11 +0100

    Add HTTP server implementation with basic API endpoints

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import http.server
+import socketserver
+import json
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API")
+        
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == "/info":
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-type", 'text/plain')
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode("utf-8"))
+        
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+PORT = 8000
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
