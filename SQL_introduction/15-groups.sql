01411522dc589bf32c6e5bfa56854d17895ff4a8
tomvieilledent <tomvieilledent@gmail.com>
2026-03-04 10:26:11 +0100

    Fix SQL query to correctly count and list records by score in second_table

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 -- List the number of records with the same score in second_table, sorted by count descending.
-SELECT score AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
