0459ec611ea0435a34e20d3e32ef142b609d2885
tomvieilledent <tomvieilledent@gmail.com>
2026-03-05 14:03:11 +0100

    Add SQL script to create states table and insert initial data

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""List all states from the database."""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """Run the SQL query and print results."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("""
+        SELECT * FROM states
+        WHERE name LIKE %s
+        ORDER BY id ASC;
+        """, (state_searched,))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
