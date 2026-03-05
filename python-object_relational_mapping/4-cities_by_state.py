08654a9eeb428d5214d7665e76a329e16809f351
tomvieilledent <tomvieilledent@gmail.com>
2026-03-05 14:52:08 +0100

    Refactor SQL query in 4-cities_by_state.py to select specific columns and correct state ID reference

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,12 +22,12 @@ def main():
     cursor = db.cursor()
 
     cursor.execute("""
-        SELECT * FROM cities
-        JOIN states ON cities.state_id=stateID
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC
         """)
-        
-
+    
     states = cursor.fetchall()
 
     for state in states:
