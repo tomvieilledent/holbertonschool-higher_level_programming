688227280da492b36e7439e5fcd874583de15bbb
tomvieilledent <tomvieilledent@gmail.com>
2026-03-05 11:27:02 +0100

    Add script to list all states from the database

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""List all states from the database."""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """Run the SQL query and print results."""
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC WHERE state = N*")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
