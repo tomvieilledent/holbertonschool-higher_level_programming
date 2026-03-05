46c666920ceeeb686721521a6bf1068a2f347b6a
tomvieilledent <tomvieilledent@gmail.com>
2026-03-05 11:32:31 +0100

    Fix SQL query to use correct table reference for ordering states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,7 @@ def main():
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHEN name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHEN name LIKE 'N%' ORDER BY state.id ASC")
 
     states = cursor.fetchall()
 
