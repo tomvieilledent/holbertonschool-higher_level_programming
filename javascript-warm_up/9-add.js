482f0f7275ebd6a8f0cef3827777b9374706ed09
tomvieilledent <tomvieilledent@gmail.com>
2026-03-19 15:52:41 +0100

    Fix: format code for better readability in object manipulation and addition functions

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,5 +3,9 @@ const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
 function add (a, b) {
-  if (isNaN(a) || isNaN(b)) { console.log('NaN'); } else { console.log(a + b); }
+  if (isNaN(a) || isNaN(b)) { console.log('NaN'); } else {
+    console.log(a + b);
+  }
 }
+
+add(a, b);
