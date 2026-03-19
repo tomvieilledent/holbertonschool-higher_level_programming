8f53c82f64a1f283352026f5f04897557f4e3a8f
tomvieilledent <tomvieilledent@gmail.com>
2026-03-19 15:22:04 +0100

    Add: implement logic to find the second largest number from command line arguments

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const arg = process.argv.slice(2);
+let max = 0;
+let second = 0;
+let i = 1;
+
+if (arg.length < 1) {
+  console.log('0');
+}
+else {
+  while (i < arg.length) {
+    if (arg[i] >= max) {
+      second = max;
+      max = arg[i];
+    }
+    if (arg[i] < max && arg[i] > second) {
+      second = arg[i];
+    }
+    i++;
+  }
+  console.log(second);
+}
