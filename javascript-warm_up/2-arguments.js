868a7083a3e8dccd695b0cc976b6742d57e1a23c
tomvieilledent <tomvieilledent@gmail.com>
2026-03-19 10:50:07 +0100

    Refactor: remove show function and directly handle argument logging

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-function show (...args) {
-  if (args.length === 0) console.log('No argument');
-  else if (args.length === 1) console.log('Argument found');
-  else console.log('Arguments found');
-}
+const args = process.argv.slice(2);
+
+if (args.length === 0) console.log('No argument');
+else if (args.length === 1) console.log('Argument found');
+else console.log('Arguments found');
