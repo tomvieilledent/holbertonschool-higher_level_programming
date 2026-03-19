495caf018a9f75edb9fbd0a7e75df8ac3adf9343
tomvieilledent <tomvieilledent@gmail.com>
2026-03-19 14:34:57 +0100

    Fix: return 1 for NaN or negative input in factorial calculation

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const a = parseInt(process.argv[2]);
 
-if (isNaN(a) || a < 0) console.log('NaN');
+if (isNaN(a) || a < 0) console.log(1);
 else if (a === 0 || a === 1) console.log(1);
 else {
   let total = 1;
