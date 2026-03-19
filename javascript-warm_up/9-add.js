99af787ade1b46a6aa5c300f8f35e0dfb2995c5e
tomvieilledent <tomvieilledent@gmail.com>
2026-03-19 14:49:53 +0100

    Refactor: encapsulate addition logic in a function

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,5 +2,9 @@
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
-if (isNaN(a) || isNaN(b)) console.log('NaN');
-else console.log(a + b);
+function add(a, b) {
+  if (isNaN(a) || isNaN(b)) 
+    console.log('NaN');
+  else 
+    console.log(a + b);
+}
