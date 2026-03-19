9709d150b5c6b648320e184ac7e602be5f2ea549
tomvieilledent <tomvieilledent@gmail.com>
2026-03-19 10:39:11 +0100

    Refactor: replace console logs with function to handle variable arguments

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+function show(...args) {
+  if (args.length === 0) console.log('No argument');
+  else if (args.length === 1) console.log('Argument found');
+  else console.log('Arguments found');
+}
