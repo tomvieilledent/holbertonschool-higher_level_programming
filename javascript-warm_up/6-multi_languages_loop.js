fe6cf2faf7fa3b54bbc132640b58a6e11dd4ecfd
tomvieilledent <tomvieilledent@gmail.com>
2026-03-19 11:44:27 +0100

    Fix: standardize quotes in 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const line of languages) console.log(line);
